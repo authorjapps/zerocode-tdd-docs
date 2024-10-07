b1b1df5fda7a6a620f367658b50bf7258bd3b232
Tineshnehete <tineshnehete000@gmail.com>
2024-10-07 18:32:28 +0530

    Fixed require() issue in ES module

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-const withNextra = require("nextra")({
+import withNextra from "nextra";
+
+const nextraConfig = withNextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
+
+export default {
+  ...nextraConfig,
+  images: {
+    unoptimized: true,
+  },
+  output: "export"
+};
 
 module.exports = {
   ...withNextra(),
