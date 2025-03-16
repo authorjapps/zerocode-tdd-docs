fa8339a03107ae83a323a5d46dbd98de7e0d7996
NirmalChandra <nirmal.fleet@gmail.com>
2025-03-16 21:19:11 +0000

    reset from main - theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -44,7 +44,6 @@ const config: DocsThemeConfig = {
  
     return (
       <>
-        
         <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
