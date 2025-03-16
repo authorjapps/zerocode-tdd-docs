464469013f01c76bf8d26907af3be860d88de372
NirmalChandra <nirmal.fleet@gmail.com>
2025-03-16 21:32:00 +0000

    Old fashion (try)

--- theme.config.tsx
+++ theme.config.tsx
@@ -37,7 +37,7 @@ const config: DocsThemeConfig = {
   docsRepositoryBase:
     "https://github.com/authorjapps/zerocode-tdd-docs/blob/main",
   footer: {
-    content: <>Zerocode TDD Docs</>,
+    text: "Zerocode-tdd Documentation",
   },
   head: () => { 
     const { frontMatter } = useConfig()
